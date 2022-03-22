f026b11a0a7c0b19393d8625703d9ba5b3c865ba
Eyrie48 <eddiie6@hotmail.com>
2022-03-22 16:28:42 -0700

    update create blade categories

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,45 +1,20 @@
 @extends('adminlte::page')
 
-@section('title', 'Categories')
+@section('title', 'To Do List')
 
 @section('content_header')
-    <h1>Category Table</h1>
+    <h1>To Do's</h1>
 @stop
 
 @section('content')
-<div class="card">
-  <div class="card-body">
-    <table id="table" class="table table-bordered">
-      <thead>
-        <tr>
-          <th>Category Name</th><th>Device Name</th><th>Serial Num</th><th>Price</th><th>Ram</th><th>Storage</th><th style="width: 40px"></th>
-        </tr>
-      </thead>
-      <tbody>
-
-        @foreach($todos AS $todo)
-        <tr>
-          <td>{{ $categories->category_name }}</td>
-          <td>{{ $categories->device_name }}</td>
-          <td>{{ $categories->serial_num }}</td>
-          <td>{{ $categories->price }}</td>
-          <td>{{ $categories->ram }}</td>
-          <td>{{ $categories->storage }}</td>
-          <td><a class="btn btn-default btn-sm" href="{{ route('todos.show',['todo'=>$todo->id]) }}">View</a></td>
-        </tr>
-        @endforeach
-
-      </tbody>
-    </table>
-  </div>
-</div>
-<a href="{{ route('todos.create') }} " class="btn btn-primary" >Create</a>
-@stop
-
-@section('js')
-<script>
-    $(document).ready(function() {
-        $('#table').DataTable();
-    } );
-</script>
+<form method="post" action="{{ route('categories.store') }}" >
+    @csrf
+    <x-adminlte-input name="category_name" label="Category Name" />
+    <x-adminlte-input name="device_name" label="Device Name" />
+    <x-adminlte-input name="serial_num" label="Serial Number" />
+    <x-adminlte-input name="price" label="Price" />
+    <x-adminlte-input name="ram" type="number" min=0 max=100 label="Ram" />
+    <x-adminlte-input name="storage" type="number" min=0 max=100 label="Storage" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
 @stop
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -11,9 +11,39 @@
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Category Name</th><th>Device Name</th><th>Serial Num</th><th>Price</th><th>Ram</th><th>Storage</th><th style="width: 40px"></th>
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach($todos AS $todo)
+        <tr>
+          <td>{{ $categories->category_name }}</td>
+          <td>{{ $categories->device_name }}</td>
+          <td>{{ $categories->serial_num }}</td>
+          <td>{{ $categories->price }}</td>
+          <td>{{ $categories->ram }}</td>
+          <td>{{ $categories->storage }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('todos.show',['todo'=>$todo->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('todos.create') }} " class="btn btn-primary" >Create</a>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
 @stop
