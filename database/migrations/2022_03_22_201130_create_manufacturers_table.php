112befb24da116520feb8eef9961115d238f9430
Eyrie48 <eddiie6@hotmail.com>
2022-03-22 23:25:46 -0700

    Update 2022_03_22_201130_create_manufacturers_table.php

--- database/migrations/2022_03_22_201130_create_manufacturers_table.php
+++ database/migrations/2022_03_22_201130_create_manufacturers_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      *
      * @return void
      */
-    
+
     public function up()
     {
         Schema::create('manufacturers', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('progress');
+            $table->integer('progress');
             $table->timestamps();
         });
     }
