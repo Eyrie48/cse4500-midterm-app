97810323a0281b5b019d304aa9ad022fa0de91f1
Eyrie48 <eddiie6@hotmail.com>
2022-03-26 21:51:38 -0700

    Update 2022_03_24_184559_create_hardwares_table.php

--- database/migrations/2022_03_24_184559_create_hardwares_table.php
+++ database/migrations/2022_03_24_184559_create_hardwares_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('hardwares', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('hardware_id');
-            $table->unsignedBigInteger('hardware_id')->references('id')->on('userinfos');
+            $table->foreign('hardware_id')->references('id')->on('userinfos');
             $table->string('invoice');
             $table->string('device_name');
             $table->float('cpu');
