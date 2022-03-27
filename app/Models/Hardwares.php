1e41a51842d01eb25a49cb6972fd9d8d7e07faba
Eyrie48 <eddiie6@hotmail.com>
2022-03-27 02:30:08 -0700

    m

--- app/Models/Hardwares.php
+++ app/Models/Hardwares.php
@@ -12,6 +12,6 @@ class Hardwares extends Model
 
     function userinfo()
     {
-        return $this->belongsTo(Userinfo::class);
+        return $this->hasMany(Userinfo::class);
     }
 }
--- database/migrations/2022_03_24_184559_create_hardwares_table.php
+++ database/migrations/2022_03_24_184559_create_hardwares_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             //$table->unsignedBigInteger('hardware_id');
             //$table->foreignId('hardware_id')->references('id')->on('userinfos')->onUpdate('cascade');
-            $table->foreignId('hardware_id')->nullable()->constrained('userinfos')->onUpdate('cascade');
+            $table->foreignId('hardware_id')->nullable()->constrained('userinfos')->onUpdate('cascade')->onDelete('cascade');
             $table->string('invoice');
             $table->string('device_name');
             $table->float('cpu');
