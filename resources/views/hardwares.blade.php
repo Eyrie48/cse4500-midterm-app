475025230aed3260c941f7036e7e65537d81c3e7
Eyrie48 <eddiie6@hotmail.com>
2022-03-27 14:43:58 -0700

    Update hardwares.blade.php

--- resources/views/hardwares.blade.php
+++ resources/views/hardwares.blade.php
@@ -17,7 +17,7 @@
             </tr>
           </thead>
           <tbody>
-            @foreach($hardwares->userinfo AS $hardware)
+            @foreach($hardwares->userinfos AS $hardware)
               <tr>
                 <td>{{ $hardware->invoice }}</td>
                 <td>{{ $hardware->device_name }}</td>
