5092f16fdc4b11b3f25a93512a675a5a98f7afa8
Eyrie48 <eddiie6@hotmail.com>
2022-03-27 03:00:05 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/userinfos', function () {
 
 Route::get('/hardwares', function () {
     $userinfo = DB::table('userinfos')->get();
-    return view('hardwares', [$userinfo]);
+    return view('hardwares', ['userinfo' => $userinfo]);
 });
 
 //test
