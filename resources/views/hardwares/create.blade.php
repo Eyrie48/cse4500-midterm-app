68d8f010c27272a3484d46bd4795e6236f027dea
Eyrie48 <eddiie6@hotmail.com>
2022-03-28 15:35:40 -0700

    Update create.blade.php

--- resources/views/hardwares/create.blade.php
+++ resources/views/hardwares/create.blade.php
@@ -11,13 +11,15 @@
     @csrf
     <x-adminlte-input name="userinfo_id" label="User ID" />
     <x-adminlte-input name="invoice" label="Invoice" />
-    <x-adminlte-select name="device_name" label="Device">
-    <x-adminlte-options :options="['Desktop', 'Laptop', 'Tablet', 'Phone']" placeholder="Select an option..." />
+    
     <x-adminlte-input name="cpu"  label="Cpu" />
     <x-adminlte-input name="price" label="Price" />
     <x-adminlte-input name="purchaseDate" type="datetime-local" label="Purchase Date"/>
     <x-adminlte-input name="ram"  label="Ram" />
     <x-adminlte-input name="storage" label="Storage" />
+    <x-adminlte-select name="device_name" label="Device" >
+    <x-adminlte-options :options="['Desktop', 'Laptop', 'Tablet', 'Phone']" 
+        placeholder="Select an option..." />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
