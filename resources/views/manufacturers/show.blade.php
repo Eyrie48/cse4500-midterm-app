c10bbf42b0f03da65fb40194ead12238ed02d1e0
Eyrie48 <eddiie6@hotmail.com>
2022-03-29 13:43:49 -0700

    Update show.blade.php

--- resources/views/manufacturers/show.blade.php
+++ resources/views/manufacturers/show.blade.php
@@ -19,6 +19,6 @@
   </div>
 
   <form style="margin: 0; padding: 0">
-    <a class="btn btn-default" href="{{ route('manufacturers.edit', ['manufacturer'=>$manufaccturer->id]) }}">Edit</a>
+    <a class="btn btn-default" href="{{ route('manufacturers.edit', ['manufacturer'=>$manufacturer->id]) }}">Edit</a>
   </form>  
 @stop
