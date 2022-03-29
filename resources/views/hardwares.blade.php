a73e9a0ad7ed5c2c3dca5b854152c06260a5200c
Eyrie48 <eddiie6@hotmail.com>
2022-03-29 15:30:20 -0700

    Update hardwares.blade.php

--- resources/views/hardwares.blade.php
+++ resources/views/hardwares.blade.php
@@ -13,7 +13,7 @@
         <table id="table" class="table table-bordered">
           <thead>
             <tr>
-            <th>User ID</th><th>Invoice#</th><th>Device Name</th><th>CPU Cores</th><th>Price</th><th>Purchase Date</th><th>Ram</th><th>Storage</th><th>View</th>
+            <th>User ID</th><th>Invoice#</th><th>Device Name</th><th>User Email</th><th>Price</th><th>Purchase Date</th><th>Ram</th><th>Storage</th><th>View</th>
             </tr>
           </thead>
           <tbody>
