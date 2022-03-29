19497dbd1f677f8f39fca327e1c72af8c06b3b20
Eyrie48 <eddiie6@hotmail.com>
2022-03-28 18:34:49 -0700

    view manufacturers

--- resources/views/manufacturers/show.blade.php
+++ resources/views/manufacturers/show.blade.php
@@ -1,12 +1,20 @@
 @extends('adminlte::page')
 
-@section('title', 'To Do List')
+@section('title', 'Manufacturer')
 
 @section('content_header')
-    <h1>To Do's</h1>
+  <h1>Manufacturers</h1>
 @stop
 
 @section('content')
-  <h2>{{ $manufacturer->title; }}</h2>
-  <div><p>{{ $manufacturer->progress; }}% finished</p></div>
+  <h2>{{ $manufacturer->company_name; }}</h2>
+  <hr>
+  <div>
+    <h4>Sales Contact Information</h4>
+    <p>{{ $manufacturer->sales_name; }}</p>
+    <p>{{ $manufacturer->sales_email; }}</p>
+    <h4>Tech Support Contact Information</h4>
+    <p>{{ $manufacturer->support_name; }}</p>
+    <p>{{ $manufacturer->support_email; }}</p>
+  </div>
 @stop
