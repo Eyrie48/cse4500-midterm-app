b3752656ec4e3767debd8ed87f815c3b8f326b41
Eyrie48 <eddiie6@hotmail.com>
2022-03-29 15:33:22 -0700

    Update hardwares.blade.php

--- resources/views/hardwares.blade.php
+++ resources/views/hardwares.blade.php
@@ -13,7 +13,7 @@
         <table id="table" class="table table-bordered">
           <thead>
             <tr>
-            <th>User ID</th><th>Invoice#</th><th>Device Name</th><th>User Email</th><th>Price</th><th>Purchase Date</th><th>Ram</th><th>Storage</th><th>View</th>
+            <th>User ID</th><th>Invoice#</th><th>Company Name</th><th>Device Name</th><th>User Email</th><th>Ram</th><th>Storage</th><th>Price</th><th>Purchase Date</th><th>View</th>
             </tr>
           </thead>
           <tbody>
@@ -21,12 +21,14 @@
               <tr>
                 <td>{{ $hardware->userinfo_id }}</td>
                 <td>{{ $hardware->invoice }}</td>
+                <td>{{ $hardware->manufacturer->caompany_name }}</td>
                 <td>{{ $hardware->device_name }}</td>
                 <td>{{ $hardware->userinfo->user_email }}</td>
-                <td>{{ $hardware->price }}</td>
-                <td>{{ $hardware->purchaseDate }}</td>
                 <td>{{ $hardware->ram }}</td>
                 <td>{{ $hardware->storage }}</td>
+                <td>{{ $hardware->price }}</td>
+                <td>{{ $hardware->purchaseDate }}</td>
+                
                 <td><a class="btn btn-default btn-sm" href="{{ route('hardwares.show',['hardware'=>$hardware->id]) }}">View</a></td>
               </tr>
             @endforeach
